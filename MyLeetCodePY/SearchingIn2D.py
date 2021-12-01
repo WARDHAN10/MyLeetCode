9c4f93681f5e1e6d9dd1e8c91c9e017af4753d32
root <codezilla@harshwardhan@gmail.com>
2021-12-01 18:25:39 +0530

    latest

--- MyLeetCodePY/SearchingIn2D.py
+++ MyLeetCodePY/SearchingIn2D.py
@@ -1,29 +1,43 @@
 def BS(arr,key):
     low,high=0,len(arr)-1
     mid = int((low+high)/2)
+    # corner case
+    if(key < arr[0][0] or key > arr[-1][1]):
+        return False
+    # find the row in which it can exist
     while(low<=high):
         mid = int((low+high)/2)
-        print(arr[mid])
-        if(arr[mid][0] > key):
-             high = mid-1
-        elif(arr[mid][-1] > key):
+        if(arr[mid][0] < key and key < arr[mid][-1]) :
+            print(mid)
+            break
+        elif(arr[mid][-1] < key):
             low = mid +1
-        else: 
-            print(arr[mid][0])
+        elif(arr[mid][0] > key):
+            high = mid-1
 
-            innerLow,innerHigh=0,len(arr[mid])-1
-            while(innerLow<=innerHigh):
-                InnerMid =int((innerLow+innerHigh)/2)
-                if(arr[InnerMid] == key):
-                    return InnerMid
-                elif(arr[InnerMid] > key): 
-                    innerHigh = InnerMid -1
+    innerLow,innerHigh=0,len(arr[mid])-1
+    while(innerLow<=innerHigh):
+        InnerMid =int((innerLow+innerHigh)/2)
+        if(arr[mid][InnerMid] == key):
+            return 'true'
+        elif(arr[mid][InnerMid] > key): 
+            innerHigh = InnerMid -1
 
-                else:
-                    innerLow = InnerMid + 1
-            return False
+        else:
+            innerLow = InnerMid + 1
+    return 'false'
 
 
 
-print(BS([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
-        
+n,m=input().split()
+matrix=[]
+for i in range(int(n)):
+    col = [int(x) for x in input().strip().split()]
+    matrix.append(col)
+
+for j in range(int(input())):
+    key =int(input())
+print(BS([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]],3))
+for j in range(int(input())):
+    key =int(input())
+    print(BS(matrix,key))
