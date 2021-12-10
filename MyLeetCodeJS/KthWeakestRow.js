6347e6236686f2de60b2ea1ace3dfcaaa4ffa036
root <codezilla@harshwardhan@gmail.com>
2021-12-10 18:17:36 +0530

    latest

---
+++ MyLeetCodeJS/KthWeakestRow.js
@@ -0,0 +1,58 @@
+const WeakRow = (mat) => {
+    let Occ = []
+    let act = []
+    for (let i = 0; i < mat.length; i++) {
+        let count = 0
+        console.log("i : {0}",i)
+        for (let j = 0; j < mat[i].length; j++) {
+            console.log("j : {0}",j)
+
+            if (mat[i][j] == 1) {
+                count += 1
+            }
+            idx = BinarySearch(Occ, count)
+            Occ.push(idx, count)
+            act.push(idx, i)
+
+
+        }
+
+
+    }
+    return Occ
+
+}
+
+
+
+
+
+
+
+const BinarySearch = (arr, k) => {
+
+    let low = 0,
+        high = arr.length - 1
+    console.log(low, high)
+    while (low <= high) {
+        let mid = parseInt((low + high) / 2)
+
+        if (arr[mid] == k) {
+            return mid
+        } else if (arr[mid] > k) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return -1
+
+}
+
+console.log(WeakRow([
+    [1, 1, 0, 0, 0],
+    [1, 1, 1, 1, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [1, 1, 1, 1, 1]
+]))
